29ee5c9dba1df5d8851f8757000516e04f6e2a8a
Ashutosh <ashutoshroy453@gmail.com>
2025-05-05 01:44:04 -0400

    modifiySignup

--- app/src/main/java/com/example/ecommerce/LoginActivity.java
+++ app/src/main/java/com/example/ecommerce/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.ecommerce;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+    public void signup(View view){
+        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+    }
+
+    public void signin(View view){
+        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+    }
+
 }
--- app/src/main/java/com/example/ecommerce/RegistrationActivity.java
+++ app/src/main/java/com/example/ecommerce/RegistrationActivity.java
@@ -1,6 +1,8 @@
 package com.example.ecommerce;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,16 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+    public void signup(View view){
+       startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+    }
+
+    public void signin(View view){
+        startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+    }
+
+
 }
