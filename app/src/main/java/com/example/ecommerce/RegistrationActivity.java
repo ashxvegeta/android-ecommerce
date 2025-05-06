4fbfaf86392eb76495378879b10fdca47cbe5b86
Ashutosh <ashutoshroy453@gmail.com>
2025-05-06 00:16:46 -0400

    added frontedn validation in singnup and connect to firebase and add data to firebase using signup

--- app/src/main/java/com/example/ecommerce/RegistrationActivity.java
+++ app/src/main/java/com/example/ecommerce/RegistrationActivity.java
@@ -2,25 +2,82 @@ package com.example.ecommerce;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegistrationActivity extends AppCompatActivity {
+
+
+    EditText name,email,password;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+//        getSupportActionBar().hide();
+        auth = FirebaseAuth.getInstance();
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.Passward);
+
     }
 
-    public void signup(View view){
-       startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+    public void signup(View view) {
+        String username = name.getText().toString().trim();
+        String useremail = email.getText().toString().trim();
+        String userpassword = password.getText().toString().trim();
+
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Enter name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(useremail)) {
+            Toast.makeText(this, "Enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(userpassword)) {
+            Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(userpassword.length()<6){
+            Toast.makeText(this, "Password must be of 6 character", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(useremail,userpassword).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+               if(task.isSuccessful()){
+                   Toast.makeText(RegistrationActivity.this, "Successfully Registred", Toast.LENGTH_SHORT).show();
+                   startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+               }else{
+                   Toast.makeText(RegistrationActivity.this, "Registration Failed"+task.getException(), Toast.LENGTH_SHORT).show();
+               }
+            }
+        });
+        // All inputs are valid, proceed
+       //  startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
     }
+
 
     public void signin(View view){
         startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
